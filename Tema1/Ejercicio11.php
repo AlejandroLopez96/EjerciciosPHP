107a3f3a54e7db85f0a8a5998b080b6ec0abbc1c
AlejandroLopez96 <alexlopezortiz9@gmail.com>
2016-09-27 10:54:56 +0200

    Update Ejercicio11.php

--- Tema1/Ejercicio11.php
+++ Tema1/Ejercicio11.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         echo "&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp"," *","<br>";
-        echo  "&nbsp","&nbsp","&nbsp","&nbsp"," *","&nbsp","&nbsp","&nbsp","*","<br>";
+        echo  "&nbsp","&nbsp","&nbsp","&nbsp","&nbsp"," *","&nbsp","&nbsp","*","<br>";
         echo "&nbsp","&nbsp","&nbsp"," *","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp"," *","<br>";
         echo "&nbsp", " *","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","*","<br>";
         echo "*********","<br>";
