9fda7b667f77bdb172094dc788bbd13d95fbdfd8
AlejandroLopez96 <alexlopezortiz9@gmail.com>
2016-10-20 09:11:58 +0200

    Tema 5 PHP

--- Tema5/Ejercicio2/index.php
+++ Tema5/Ejercicio2/index.php
@@ -1,50 +1,71 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <form action="index.php" method="get">
-            Número:  <input type="number" name="numero" autofocus>
-            <input type="submit" value="Mostrar">
+  <head>
+    <meta charset="UTF-8">
+        <title>Muestra máximos y mínimos</title>
+
+  </head>
+  <body>
+    <?php
+      $n =  $_GET['n'];
+      $cuentaNumeros = $_GET['cuentaNumeros'];
+      $maximo = $_GET['maximo'];
+      $minimo = $_GET['minimo'];
+      $numeros = $_GET['numeros'];
+      
+      
+      if (!isset($n)){
+        //Primera visita
+        $cuentaNumeros = 0;
+        $maximo = PHP_INT_MIN;
+        $minimo = PHP_INT_MAX;
+        $numeros = "";
+      }
+      
+      if(isset($n) && $cuentaNumeros <=10){
+        //Segunda y posteriores visitas
+        $cuentaNumeros++;
+        if($n > $maximo){
+          $maximo = $n; 
+        }
+        if($n < $minimo){
+          $minimo = $n;
+        }
+        
+        $numeros .= " " . $n;
+      }
+      
+      if ($cuentaNumeros == 10){
+        //última visita
+        $arrayNumeros = explode(" ", $numeros);
+        echo "Los números introducidos son: <br>";
+        foreach ($arrayNumeros as $valor) {
+          echo $valor;
+          if($valor == $maximo){
+            echo " máximo";
+          }
+          
+           if($valor == $minimo){
+            echo " mínimo";
+          }
+          echo "<br>";
+        }
+        
+      }else{
+        ?>  
+        <form action="Ejercicio2.php" method="get">
+        Número <input type="number" name="n"><br>
+        <input type="hidden" name="cuentaNumeros" value="<?php echo $cuentaNumeros; ?>">
+         <input type="hidden" name="maximo" value="<?php echo $maximo; ?>">
+         <input type="hidden" name="minimo" value="<?php echo $minimo; ?>">
+         <input type="hidden" name="numeros" value="<?php echo $numeros; ?>">
+         <input type="submit" value="Aceptar">
         </form>
-        <?php
-            $numero;
-            $maximo = PHP_INT_MIN;
-            $minimo = PHP_INT_MAX;
-            $i;
+      <?php  
+      }
+  
+    ?>
+    
+  </body>
 
-            for ($i = 0; $i < 10; $i++) {
-              $numero[$i] = $_GET["numero"];
-
-              if ($numero[$i] < $minimo) {
-                $minimo = $numero[$i];
-              }
-
-              if ($numero[$i] > $maximo)	{
-                $maximo = $numero[$i];
-              }
-            }
-
-            echo "<br>";
-
-            for ($i = 0; $i < 10; $i++) {
-              echo $numero[$i];
-              if ($numero[$i] == $maximo) {
-                echo " máximo";
-              }
-
-              if ($numero[$i] == $minimo) {
-                echo " mínimo";
-              }
-              echo "<br>";
-            }
-        ?>
-    </body>
 </html>
